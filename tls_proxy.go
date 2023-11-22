777bcb62baab2d84ef36351b0892d4d9341858d4
jebeaudet <jebeaudet@gmail.com>
2023-11-21 19:41:57 -0500

    Tentative fix for gh-41

--- tls_proxy.go
+++ tls_proxy.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/caddyserver/certmagic"
 )
@@ -40,8 +41,6 @@ func ProxyTcp(conn net.Conn, addr string, port int, useTls bool, certConfig *cer
 
 func handleConnection(conn net.Conn, upstreamAddr string, port int) {
 
-	defer conn.Close()
-
 	useTls := false
 	addr := upstreamAddr
 
@@ -63,26 +62,26 @@ func handleConnection(conn net.Conn, upstreamAddr string, port int) {
 	}
 
 	if err != nil {
-		log.Print(err)
+		log.Println("Error when establishing connection:", err)
+		conn.Close()
 		return
 	}
 
-	defer upstreamConn.Close()
+	log.Println("Starting proxying")
+	deadline := time.Now().Add(5 * time.Hour)
+	conn.SetDeadline(deadline)
+	upstreamConn.SetDeadline(deadline)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
 
 	// Copy request to upstream
 	go func() {
+		defer upstreamConn.Close()
+		defer conn.Close()
 		_, err := io.Copy(upstreamConn, conn)
 		if err != nil {
-			log.Println(err.Error())
-		}
-
-		if c, ok := upstreamConn.(*net.TCPConn); ok {
-			c.CloseWrite()
-		} else if c, ok := upstreamConn.(*tls.Conn); ok {
-			c.CloseWrite()
+			log.Printf("Error when copying request to upstream (%s:%d): %s", upstreamAddr, port, err)
 		}
 
 		wg.Done()
@@ -90,19 +89,14 @@ func handleConnection(conn net.Conn, upstreamAddr string, port int) {
 
 	// Copy response to downstream
 	go func() {
+		defer upstreamConn.Close()
+		defer conn.Close()
 		_, err := io.Copy(conn, upstreamConn)
-		//conn.(*net.TCPConn).CloseWrite()
 		if err != nil {
-			log.Println(err.Error())
+			log.Printf("Error when copying response to downstream (%s:%d): %s", upstreamAddr, port, err)
 		}
-		// TODO: I added this to fix a bug where the copy to
-		// upstreamConn was never closing, even though the copy to
-		// conn was. It seems related to persistent connections going
-		// idle and upstream closing the connection. I'm a bit worried
-		// this might not be thread safe.
-		conn.Close()
 		wg.Done()
 	}()
-
 	wg.Wait()
+	log.Println("All done")
 }
